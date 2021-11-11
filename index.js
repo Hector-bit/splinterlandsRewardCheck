a8079c56b0efa46faeb41dc16ff5f79c81c9c9ff
Hector-bit <hectormartinez.2978@gmail.com>
2021-11-11 06:28:31 -0800

    added axios and index now returns player info

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
+
+const axios = require('axios');
 let account = require('./account.json').account;
 let accountDetails = [];
 
-async function getStats(username, key){
-    try {
-        console.log('works ', username, key);
-        const data = await fetch(`https://api.splinterlands.io/players/quests?username=${key}`);
-        accountDetails.push(data);
-    }
-    catch{
-        console.log(`Could not get info from account: ${username}`)
-    }
+function getStats(username, key){
+    let data = axios.get(`https://api.splinterlands.io/players/quests?username=${username}`);
+    return data;
 }
 
-for(i = 0; i<account.length; i++){
-    // console.log(accountKeys[i], 'bitch');
-    getStats(account[i][0], account[i][1]);
+async function start(){
+
+    for(i = 0; i<account.length; i++){
+        // console.log(accountKeys[i], 'bitch');
+        let response = await getStats(account[i][0], account[i][1]);
+        accountDetails.push(response);
+    }
+    console.log('HERE YOU GO: ', accountDetails)
 }
 
-console.log(`CONGRATS: ${accountDetails}`);
+start();
