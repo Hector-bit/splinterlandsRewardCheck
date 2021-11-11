58696b3c880a0bc2d85569a4b478c98aeaa92e5b
Hector-bit <hectormartinez.2978@gmail.com>
2021-11-11 07:16:48 -0800

    done with card collection of daily rewards and added some design

--- index.js
+++ index.js
@@ -3,19 +3,31 @@ const axios = require('axios');
 let account = require('./account.json').account;
 let accountDetails = [];
 
-function getStats(username, key){
-    let data = axios.get(`https://api.splinterlands.io/players/quests?username=${username}`);
-    return data;
+function getStats(username){
+    axios.get(`https://api.splinterlands.io/players/quests?username=${username}`)
+    .then(response => {
+        return response.data[0].rewards
+    })
+    .catch(error => {
+        console.log('could not find user: ', username);
+        console.log('THE ERROR: ', error);
+    })
 }
 
 async function start(){
-
+    accounts = 0
     for(i = 0; i<account.length; i++){
         // console.log(accountKeys[i], 'bitch');
-        let response = await getStats(account[i][0], account[i][1]);
-        accountDetails.push(response.data[0].rewards);
+        let response = await getStats(account[i][0]);
+        if(response != undefined){
+            accounts += 1
+            accountDetails.push(response);   
+        }
     }
-    console.log('nothing yet', accountDetails)
+    console.log(`Out of ${accounts} you collected the following`)
+    console.log('===============================================')
+    console.log(accountDetails)
+    console.log('===============================================')
 }
 
 start();
